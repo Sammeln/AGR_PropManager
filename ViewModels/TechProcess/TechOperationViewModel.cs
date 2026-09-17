140d32446d728c9f650fb81e0d9483c32ce54e1e
Sammeln <40733234+Sammeln@users.noreply.github.com>
2026-09-17 16:43:45 +0300

    Fix operation VM null entity handling for scoped persistence

--- ViewModels/TechProcess/TechOperationViewModel.cs
+++ ViewModels/TechProcess/TechOperationViewModel.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AGR_PropManager.ViewModels.Base;
 using AGR_PropManager.ViewModels.Components;
-using AgroventInfrastructure.Interfaces.Entities.TechProcess;
 using AgroventInfrastructure.Entities.TechProcess;
 
 namespace AGR_PropManager.ViewModels.TechProcess
@@ -16,15 +9,10 @@ namespace AGR_PropManager.ViewModels.TechProcess
     {
         public Operation? Operation { get; }
 
-        #region CTOR
-        public TechOperationViewModel()
-        {
-
-        }
+        public TechOperationViewModel() { }
 
         public TechOperationViewModel(Operation operation)
         {
-
             Operation = operation;
             WorkstationName = operation.WorkstationName;
             Name = operation.Name;
@@ -39,35 +27,19 @@ namespace AGR_PropManager.ViewModels.TechProcess
             Name = operation.Name;
             CostPerHour = operation.CostPerHour;
         }
-        #endregion
 
-        #region PROPS
-
-
-        #region Property - 
         private ComponentItemViewModel _ParentComponent;
         public ComponentItemViewModel ParentComponent
         {
             get => _ParentComponent;
             set => Set(ref _ParentComponent, value);
         }
-        #endregion 
 
-        #region WorkstationName
         private string _WorkstationName;
         public string WorkstationName { get => _WorkstationName; set => Set(ref _WorkstationName, value); }
-        #endregion
 
-        #region Property - Name
         private string _Name;
-        public string Name
-        {
-            get => _Name;
-            set => Set(ref _Name, value);
-        }
-        #endregion
-
-        #region Property - CostPerHour
+        public string Name { get => _Name; set => Set(ref _Name, value); }
 
         private decimal _CostPerHour;
         public decimal CostPerHour
@@ -75,35 +47,28 @@ namespace AGR_PropManager.ViewModels.TechProcess
             get => _CostPerHour;
             set
             {
-                if (Set(ref _CostPerHour, value))
-                {
+                if (!Set(ref _CostPerHour, value)) return;
+
+                // Template VM не имеет связанной EF-сущности. Для обычной операции
+                // обновляем только локальный snapshot, а ParentComponent выполняет DB write
+                // через новый scope.
+                if (Operation != null)
                     Operation.CostPerHour = value;
-                    ParentComponent?.OnOperationCostChanged(this);
-                }
+
+                ParentComponent?.OnOperationCostChanged(this);
             }
         }
-        #endregion
 
-        #region Property - _SequenceNumber
         private int _SequenceNumber;
-        public int SequenceNumber
-        {
-            get => _SequenceNumber;
-            set => Set(ref _SequenceNumber, value);
-        }
-        #endregion
+        public int SequenceNumber { get => _SequenceNumber; set => Set(ref _SequenceNumber, value); }
 
-        #region Property - TechProcess
         private TechnologicalProcess _TechProcess;
         public TechnologicalProcess TechProcess
         {
             get => _TechProcess;
             set => Set(ref _TechProcess, value);
         }
-        #endregion 
-        #endregion
 
         public event EventHandler? CostChanged;
-
     }
 }
